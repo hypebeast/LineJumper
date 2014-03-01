e2acc90ddc140a47947631ec4ff738c54a86a9bc
Sebastian Ruml <sebastian.ruml@gmail.com>
2014-03-01 13:42:10 +0100

    Initial import

---
+++ line_jumper.py
@@ -0,0 +1,30 @@
+import sublime, sublime_plugin
+
+class LineJumperCommand(sublime_plugin.TextCommand):
+    def run(self, edit, **args):
+        number_of_lines = 10
+        cmd = 'move'
+        forward = False
+        cmd_args = {}
+
+        if not ('cmd' in args):
+            return
+
+        if 'number_of_lines' in args:
+            number_of_lines = args['number_of_lines']
+
+        if args['cmd'] == 'up':
+            cmd = 'move'
+            cmd_args = {'by': 'lines', 'forward': False}
+        elif args['cmd'] == 'down':
+            cmd = 'move'
+            cmd_args = {'by': 'lines', 'forward': True}
+        elif args['cmd'] == 'up_select':
+            cmd = 'move'
+            cmd_args = {'by': 'lines', 'forward': False, 'extend': True}
+        elif args['cmd'] == 'down_select':
+            cmd = 'move'
+            cmd_args = {'by': 'lines', 'forward': True, 'extend': True}
+
+        for i in range(number_of_lines):
+            self.view.run_command(cmd, cmd_args)
